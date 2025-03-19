5cefa0a71d04e40a1bedcd6c76919aa1483a5497
AyushRaj07 <74702706+AyushRaj07@users.noreply.github.com>
2025-03-19 14:23:12 +0000

    commit

--- Intro.java
+++ Intro.java
@@ -1,6 +1,6 @@
 //JVM has following:
 
-//classLoader: it scans all the classes in the file
+//classLoader: it locates all the .class files, read the bytecode and converts the bytecode into a class object.
 //Runtime memory areas - method, heap, stack, pc register, native method stack
 //execution engine - interpreter, JIT compiler, Garbage collector
 
@@ -9,3 +9,8 @@
 //native method is a method that is implemented in language other than java, typically in c or c++. These methods allow java applications to interact with platform specific functionalities
 
 //JIT Compiler - it detects frequently executed bytecode and compiles it into native machine code for faster execution.
+
+//bytecode consist of opcodes and operands
+
+//The classLoader converts the bytecode into a class object, which repressnts the class in the jvm.
+
